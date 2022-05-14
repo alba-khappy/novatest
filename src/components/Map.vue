c4a67003ee89f59926ca2c0ca35bf3efab25f53c
albinabist1993@mail.ru <b.hapilin@gateline.ru>
2022-05-15 00:48:18 +0300

    Верстка структуры, вставка слайдера

---
+++ src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+    <yandex-map
+            :coords="coords"
+            :zoom="10"
+            @click="onClick"
+    >
+        <ymap-marker
+                :coords="coords"
+                marker-id="123"
+                hint-content="some hint"
+        />
+    </yandex-map>
+</template>
+
+
+<script>
+
+    export default {
+        data: () => ({
+            coords: [
+                54.82896654088406,
+                39.831893822753904,
+            ],
+        }),
+        methods: {
+            onClick(e) {
+                this.coords = e.get('coords');
+            },
+        },
+    };
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/Swiper.vue
@@ -0,0 +1,70 @@
+<template>
+    <swiper :options="swiperOptions">
+        <swiper-slide>
+            <img src="../assets/section-actors-1.jpg">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/section-actors-2.jpg">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/section-actors-3.jpg">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/section-actors-4.jpg">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/section-actors-1.jpg">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/section-actors-2.jpg">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/section-actors-3.jpg">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="../assets/section-actors-4.jpg">
+        </swiper-slide>
+
+    </swiper>
+</template>
+
+<script>
+    import 'swiper/dist/css/swiper.css'
+    import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+    export default {
+        name: 'Swiper',
+        components: {
+            swiper,
+            swiperSlide
+        },
+        data() {
+            return {
+                swiperOptions : {
+                    slidesPerView: 5,
+                    spaceBetween: 24,
+                    freeMode: true,
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev'
+                    }
+                }
+            }
+        }
+    }
+
+</script>
+
+
+<style scoped>
+    .swiper-slide{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .swiper-container {
+        height : 450px;
+        width : 100%;
+    }
+
+</style>
